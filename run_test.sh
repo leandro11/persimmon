5908b701f3a0763bd23bb646648915b95fc0ed93
zhangnan-aa <nan@appannie.com>
2015-04-25 02:00:04 +0800

    Add test cases for refactor of member dashboard

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
 export DJANGO_SETTINGS_MODULE=TTMS.settings
+
 python -m unittest tests.units.utils.test_user
 python -m unittest tests.units.management.test_staff_view
+python -m unittest tests.units.member.test_member_view
