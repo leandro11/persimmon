ab76fa9cf483652f6f39778f710fbcbda810c359
zhangnan-aa <nan@appannie.com>
2015-05-03 15:59:41 +0800

    Add testcases

--- run_test.sh
+++ run_test.sh
@@ -6,3 +6,4 @@ python -m unittest tests.units.utils.test_user
 python -m unittest tests.units.management.test_staff_view
 python -m unittest tests.units.management.test_staff_register
 python -m unittest tests.units.member.test_member_view
+python -m unittest tests.units.transaction.test_transaction_view
---
+++ tests/units/transaction/test_transaction_view.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+
+from unittest import TestCase
+
+from mock import MagicMock, patch
+
+
+class TransactionViewTest(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_add_transaction(self):
+        self.assertTrue(True)
